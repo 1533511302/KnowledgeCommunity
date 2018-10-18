d615611cf224615b734c6fa540a897f769aca786
1533511302 <34854372+1533511302@users.noreply.github.com>
2018-10-18 09:14:43 +0800

    补提交TopicClass实体类

---
+++ src/main/java/top/maniy/entity/Topicclass.java
@@ -0,0 +1,49 @@
+package top.maniy.entity;
+
+
+public class Topicclass {
+
+  private Integer id;
+  private String topicClassName;
+  private String topicClassDescribe;
+  private String photo;
+
+
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+
+  public String getTopicClassName() {
+    return topicClassName;
+  }
+
+  public void setTopicClassName(String topicClassName) {
+    this.topicClassName = topicClassName;
+  }
+
+
+  public String getTopicClassDescribe() {
+    return topicClassDescribe;
+  }
+
+  public void setTopicClassDescribe(String topicClassDescribe) {
+    this.topicClassDescribe = topicClassDescribe;
+  }
+
+
+  public String getPhoto() {
+    return photo;
+  }
+
+  public void setPhoto(String photo) {
+    this.photo = photo;
+  }
+
+
+}
