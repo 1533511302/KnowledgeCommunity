84288f01b83b1653ba19286a3b597728458f1cca
1533511302 <34854372+1533511302@users.noreply.github.com>
2018-10-26 11:59:25 +0800

    commentMapper

--- src/main/java/top/maniy/mapper/CommentMapper.java
+++ src/main/java/top/maniy/mapper/CommentMapper.java
@@ -1,6 +1,7 @@
 package top.maniy.mapper;
 
 import org.apache.ibatis.annotations.Param;
+import top.maniy.entity.Comment;
 
 import java.util.List;
 
@@ -10,14 +11,14 @@ import java.util.List;
  * @Description:
  * @date 2018/10/26 11:31
  */
-public interface Comment {
+public interface CommentMapper {
 
     //根据massageId查询评论
     List<Comment> findCommentByMassageId(Integer massageId);
 
     //根据massageId查询评论依据点赞量降序排列取前num
-    List<Comment> findCommentByMassageIdAndByLikeNumbDescToNum(@Param("categoryId") Integer categoryId,@Param("likeNumb")
-                                                               Integer likeNumb,@Param("num") Integer num);
+    List<Comment> findCommentByMassageIdAndByLikeNumbDescToNum(@Param("categoryId") Integer categoryId, @Param("likeNumb")
+                                                               Integer likeNumb, @Param("num") Integer num);
 
     //添加评论
     boolean saveComment(Comment comment);
